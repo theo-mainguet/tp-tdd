4ff0db91cfa361b878a49af42c4fe61b44eeac88
Théo Mainguet <theo.mainguet@yahoo.fr>
2016-04-22 16:03:31 +0200

    troisieme depot

--- src/main/java/iut/tdd/RomanNumerals.java
+++ src/main/java/iut/tdd/RomanNumerals.java
@@ -2,42 +2,39 @@ package iut.tdd;
 
 public class RomanNumerals {
 	public String convertToRoman(String arabe) {
-		String rom = "";
-		int I=0;
-		int nbr = Integer.parseInt(arabe);
-		int nbr2=nbr;
-		if(nbr>=10){
-			for(int i=0; i<(nbr/10);i++){
-				rom+="X";
-				nbr2=nbr-10;
+		int[] tab = new int[]{1000, 500, 100, 50, 10, 5};
+		String[] res = new String[]{"M", "D,", "C", "L", "X", "V"};
+		
+		
+		int initial = Integer.valueOf(arabe);
+		int next = initial;
+		String result = "";
+		
+		for (int i=0; i<tab.length;i++) {
+			do {
+				next = initial % tab[i];
+				if (next < initial) {
+					result += res[i];
+					initial -= tab[i];
+				}
+			} while (initial % tab[i] >= tab[i]);
+			
+			if (initial == tab[i]-1) {
+				initial = initial - (tab[i]-1);
+				result += "I" + res[i];
 			}
 		}
-			if (nbr2 <4){
-				for(int i=0; i<nbr2; i++){
-					rom+="I";
-				}	
-			}else if (nbr2%5==4){
-				rom+= "I";
-				if(((nbr2/5)<1)){
-					rom+="V";
-				}else{
-					rom+="X";
-				}
-			}else if (nbr2>=5 && nbr2<=8){
-				rom+="V";
-				for(int i=0; i<(nbr2-5);i++){
-					rom+="I";
-				}
-			}
-			
 		
 		
-		
-		return rom;
+		if (initial == 2) {
+			return "II";
+		} else if (initial == 1) {
+			return "I";
+		}
+		return result;
 	}
 
 	public String convertFromRoman(String roman) {
-		
 		return "1";
 	}
 
